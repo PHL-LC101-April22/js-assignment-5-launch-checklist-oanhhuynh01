8bb1c7bfea14e782432bcc83ad33ebcc3eb4d313
Oanh Huynh <oanhhuynh01@gmail.com>
2022-07-07 15:26:14 -0400

    first commit

--- script.js
+++ script.js
@@ -12,5 +12,13 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
-   
+   let form = document.querySelector("testForm");
+   form.addEventListener("Submit", function(event) {
+    let pilot = document.querySelector("input[name=pilotName]");
+    let copilot = document.querySelector("input[name=copilotName");
+    let fuelLevel = document.querySelector("input[name=fuelLevel");
+    let cargoLevel = document.querySelector("input[name=cargoMass");
+   formSubmission(document, pilot, copilot, fuelLevel, cargoLevel);
+   });
+
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,11 +17,40 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
+    let validationReturn;
+    if (testInput == ""){
+        validationReturn = "Empty";
+    }
+    if (isNaN(testInput)){
+        validationReturn == "Is a Number";
+    }
+    if (isNaN(testInput) == false){
+        validationReturn == "Not a Number";
+    }
+    return validationReturn;
    
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+function formSubmission(document, pilot, copilot, fuelLevel, cargoLevel) {
+   let form = document.querySelector("testForm");
+   form.addEventListener("Submit", function(event){
+        if (pilot === "" || copilot === "" || fuelLevel === "" || cargoMass === ""){
+            alert("All fields are required!");
+            event.preventDefault();
+        }
+        if (fuelLevel < 10000){
+            let faultyItems = document.getElementByID("faultyItems");
+            faultyItems.styles.visibility = "visible";
+            document.getElementByID("fuelStatus").innerHTML = 'Not Enough fuel for the journey';
+            document.getElementByID("launchStatus").innerHTML = 'Shuttle not ready for launch';
+            document.getElementByID("launchStatus").styles.color = "red";
+        }
+        document.getElementByID("pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`;
+        document.getElementByID("copilotStatus").innerHTML = `Co-pilot ${copilot} is ready for launch`;
+
+   });
    
+  
 }
 
 async function myFetch() {
